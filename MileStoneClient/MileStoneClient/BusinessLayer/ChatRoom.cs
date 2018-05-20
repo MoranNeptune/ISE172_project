bbec17d67a3b528bdfa0d1326026e16f68b39161
unknown <edenta@post.bgu.ac.il>
2018-05-20 17:32:46 +0300

    updates ChatRoom.cs

--- MileStoneClient/MileStoneClient/BusinessLayer/ChatRoom.cs
+++ MileStoneClient/MileStoneClient/BusinessLayer/ChatRoom.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MileStoneClient.CommunicationLayer;
 using MileStoneClient.PresistentLayer;
+using MileStoneClient.PresentationLayer;
 
 namespace MileStoneClient.BusinessLayer
 {
@@ -17,12 +18,17 @@ namespace MileStoneClient.BusinessLayer
         private MessageHandler allMessages;
         private UserHandler allUsers;
         private IdHandler groupsId;
+        private List<GuiMessage> presMsgs;
+        private PresentationLayer.Action sort, filter;
 
         // constractors
         public ChatRoom(string url)
         {
             this.url = url;
             this.currUser = null;
+            presMsgs = new List<GuiMessage>();
+            sort = new SortByTime();
+            filter = null;
             allMessages = new MessageHandler("allMessages");
             allUsers = new UserHandler("allUsers");
             for (int i = 0; i < allUsers.List.Count; i++)
@@ -46,7 +52,6 @@ namespace MileStoneClient.BusinessLayer
                 return true;
             return false;
         }
-
 
         /// <summary>
         /// Find a specific user
@@ -78,53 +83,16 @@ namespace MileStoneClient.BusinessLayer
         }
 
         /// <summary>
-        /// the function retrieve 10 messages from the server and saves the new messages that we got.
+        /// return list of the users in a given group
         /// </summary>
-        /// <returns>List of the last 10 messages that were retrived</returns>
-        public List<Message> retrieveMessages()
+        /// <param name="g_id">gourp id</param>
+        /// <returns>List of the group's members</returns>
+        public List<string> getMembersOf(string g_id)
         {
-            // retrives last 10 messages from the server
-            List<IMessage> msgs = Communication.Instance.GetTenMessages(this.url);
-            List<Message> msgToUpdate = new List<Message>();
-            // check which messages we should update in our files
-            for (int i = 0; i < msgs.Count; i++)
-            {
-                Message newMsg;
-                User newUser = findUser(msgs[i].UserName, msgs[i].GroupID);
-                // if the user already exist in the list
-                if (newUser != null)
-                {
-                    newMsg = new Message(msgs[i].MessageContent, msgs[i].Date, msgs[i].Id, newUser);
-                }
-                // if the user doest not exist on our lists.
-                // and updates the lists with new group id, users and messages
-                else
-                {
-                    ID newId = findGroupId(msgs[i].GroupID);
-                    // if the group id already exist on the system
-                    if (newId == null)
-                    {
-                        newId = new ID(msgs[i].GroupID);
-                        this.groupsId.updateFile(newId);
-                    }
-                    newUser = new User(msgs[i].UserName, newId);
-                    newId.addMember(newUser.Nickname);
-                    this.allUsers.updateFile(newUser);
-                    newMsg = new Message(msgs[i].MessageContent, msgs[i].Date, msgs[i].Id, newUser);
-                }
-
-                // check if the message aleardy exist 
-                if (!this.allMessages.List.Contains(newMsg))
-                {
-                    newUser.addMessage(newMsg);
-                    msgToUpdate.Add(newMsg);
-                }
-            }
-            // update messages file
-            if (msgToUpdate.Count > 0)
-                this.allMessages.updateFile(msgToUpdate);
-            int index = this.allMessages.List.Count;
-            return this.allMessages.List.GetRange(index - 10, 10);
+            ID id = findGroupId(g_id);
+            if (id != null)
+                return id.Members;
+            return null;
         }
 
         /// <summary>
@@ -140,42 +108,11 @@ namespace MileStoneClient.BusinessLayer
             if (msg != null)
             {
                 allMessages.updateFile(msg);
+                // הוספתי
+                presMsgs.Add(new GuiMessage(msg.Body, msg.DateTime, msg.Id, msg.User.Nickname, msg.User.G_id.idNumber));
                 return true;
             }
             return false;
-        }
-
-        /// <summary>
-        /// returns all messages of a certain user
-        /// </summary>
-        /// <param name="nickname">the user's nickname</param>
-        /// <param name="g_id">the user's group id</param>
-        /// <returns>list of all the messages of the certain user</returns>
-        public List<Message> displayAll(string nickname, string g_id)
-        {
-            // Find the specific user
-            User user = findUser(nickname, g_id);
-            if (user != null)
-                return user.msgHandler.List;
-            return null;
-        }
-
-        /// <summary>
-        /// retrive new messages from the server
-        /// </summary>
-        /// <param name="num">number of messages the user would like to display</param>
-        /// <returns>returns a specific number of messages</returns>
-        public List<Message> display(int num)
-        {
-            if (num > this.allMessages.List.Count)
-                return this.allMessages.List;
-            else
-            {
-                List<Message> msgDisplay = new List<Message>(num);
-                msgDisplay = this.allMessages.List.GetRange(allMessages.List.Count - num, num);
-                //  msgDisplay.AddRange(this.allMessages.List.GetRange((this.allMessages.List).Count - num, (this.allMessages.List).Count - 1));
-                return msgDisplay;
-            }
         }
 
         /// <summary>
@@ -234,5 +171,171 @@ namespace MileStoneClient.BusinessLayer
             get { return currUser; }
             set { currUser = value; }
         }
+
+        // לעדכן ************************************************************************************************************
+        public List<GuiMessage> getMessages(int order, List<PresentationLayer.Action> actions)
+        {
+            // if the sort or filter had changed, update all the messages from the files
+            if (this.sort != actions[0] | this.filter != actions[1])
+            {
+                updatePresMessages();
+                this.sort = actions[0];
+                this.filter = actions[1];
+            }
+            //retrieveMessages();
+            if (filter != null)
+                filter.action(presMsgs);
+
+            if (sort != null)
+            {
+                // ascending sort - defult sort
+                sort.action(presMsgs);
+
+                // if descending sort
+                if (order == 1)
+                {
+                    presMsgs.Reverse();
+                }
+            }
+
+            return presMsgs;
+        }
+
+        /// <summary>
+        /// the function retrieve 10 messages from the server and saves the new messages that we got, in both lists
+        /// </summary>
+        public void retrieveMessages()
+        {
+            // retrives last 10 messages from the server
+            List<IMessage> msgs = Communication.Instance.GetTenMessages(this.url);
+            List<Message> msgToUpdate = new List<Message>();
+            // List<GuiMessage> msgToRetrive = new List<GuiMessage>();
+            // check which messages we should update in our files
+            for (int i = 0; i < msgs.Count; i++)
+            {
+                Message newMsg;
+                User newUser = findUser(msgs[i].UserName, msgs[i].GroupID);
+                // if the user already exist in the list
+                if (newUser != null)
+                {
+                    newMsg = new Message(msgs[i].MessageContent, msgs[i].Date, msgs[i].Id, newUser);
+                }
+                // if the user doest not exist on our lists.
+                // and updates the lists with new group id, users and messages
+                else
+                {
+                    ID newId = findGroupId(msgs[i].GroupID);
+                    // if the group id already exist on the system
+                    if (newId == null)
+                    {
+                        newId = new ID(msgs[i].GroupID);
+                        this.groupsId.updateFile(newId);
+                    }
+                    newUser = new User(msgs[i].UserName, newId);
+                    newId.addMember(newUser.Nickname);
+                    allUsers.updateFile(newUser);
+                    newMsg = new Message(msgs[i].MessageContent, msgs[i].Date, msgs[i].Id, newUser);
+                }
+
+                // check if the message aleardy exist 
+                if (!allMessages.List.Contains(newMsg))
+                {
+                    newUser.addMessage(newMsg);
+                    msgToUpdate.Add(newMsg);
+                    // הוספתי
+                    presMsgs.Add(new GuiMessage(newMsg.Body, newMsg.DateTime, newMsg.Id, newMsg.User.Nickname, newMsg.User.G_id.idNumber));
+                }
+            }
+            // update messages file
+            if (msgToUpdate.Count > 0)
+            {
+                allMessages.updateFile(msgToUpdate);
+                // הוספתי
+                // presMsgs.AddRange(msgToRetrive);
+            }
+            //  int index = allMessages.List.Count;
+            //  שורות שינוי 2
+            // return this.allMessages.List.GetRange(index - 10, 10);
+            // return presMsgs.GetRange(index - 10, 10);
+        }
+
+        /// <summary>
+        /// updates all the messages from the files to the presentation messages
+        /// </summary>
+        private void updatePresMessages()
+        {
+            if (allMessages.List != null)
+                for (int i = 0; i < allMessages.List.Count; i++)
+                {
+                    Message m = allMessages.List[i];
+                    presMsgs.Add(new GuiMessage(m.Body, m.DateTime, m.Id, m.User.Nickname, m.User.G_id.idNumber));
+                }
+        }
+
+        // 19.5.18
+
+        /// <returns>list of all the messages of the certain user</returns>
+        public List<string> getNicknames()
+        {
+            List<string> nicknames = new List<string>();
+            nicknames.Add("Users");
+            for (int i = 0; i < allUsers.List.Count; i++)
+                nicknames.Add(allUsers.List[i].Nickname);
+            return nicknames;
+        }
+
+        /// <returns>list of all the messages of the certain user</returns>
+        public List<string> getGroups()
+        {
+            List<string> grp = new List<string>();
+            grp.Add("Groups");
+            for (int i = 0; i < groupsId.List.Count; i++)
+                grp.Add(groupsId.List[i].idNumber);
+            return grp;
+        }
+
+        /// <summary>
+        /// returns all messages of a certain user
+        /// </summary>
+        /// <param name="nickname">the user's nickname</param>
+        /// <param name="g_id">the user's group id</param>
+        /// <returns>list of all the messages of the certain user</returns>
+        /*  public List<Message> displayAll(string nickname, string g_id)
+          {
+              // Find the specific user
+              User user = findUser(nickname, g_id);
+              if (user != null)
+                  return user.msgHandler.List;
+              return null;
+          }*/
+        // החלפתי את הפונקציה שלמעלה
+        /*   public List<GuiMessage> displayAll(string nickname, string g_id)
+           {
+               // Find the specific user
+               User user = findUser(nickname, g_id);
+               if (user != null)
+                   return (new FilterByUser(nickname, g_id)).action(presMsgs);
+               return null;
+           }
+
+
+           /// <summary>
+           /// retrive new messages from the server
+           /// </summary>
+           /// <param name="num">number of messages the user would like to display</param>
+           /// <returns>returns a specific number of messages</returns>
+           public List<Message> display(int num)
+           {
+               if (num > this.allMessages.List.Count)
+                   return this.allMessages.List;
+               else
+               {
+                   List<Message> msgDisplay = new List<Message>(num);
+                   msgDisplay = this.allMessages.List.GetRange(allMessages.List.Count - num, num);
+                   return msgDisplay;
+               }
+           }
+           */
+
     }
 }
