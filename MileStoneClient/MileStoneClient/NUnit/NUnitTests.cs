12d7388d9b83f8b5ae7256238cbd81c835b6a29f
ירין כהן <yarinc@auth.ad.bgu.ac.il>
2018-05-21 14:44:33 +0300

    Adding NUnit tests

---
+++ MileStoneClient/MileStoneClient/NUnit/NUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MileStoneClient.BusinessLayer;
+
+namespace MileStoneClient.NUnit
+{
+    [TestFixture]
+    class NUnitTests
+    {
+        [TestCase]
+        public void RegisterValid()
+        {
+            ChatRoom c = new ChatRoom("http://ise172.ise.bgu.ac.il:80");
+            string name = "name1";
+            string group = "1";
+
+            Assert.AreEqual(true, c.register(name, group));
+            
+        }
+
+    }
+}
