131408f49a6a1eba667fb022cb2152cf1fc0f908
עדן תם <edenta@auth.ad.bgu.ac.il>
2018-06-13 21:25:53 +0300

    add class SqlParam

---
+++ MileStoneClient/MileStoneClient/PresistentLayer/SqlParam.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileStoneClient.PresistentLayer
+{
+    class SqlParam
+    {
+        private string name;
+        private string data;
+
+        public SqlParam(string name, string data)
+        {
+            this.name = name;
+            this.data = data;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Value
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+}
